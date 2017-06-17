202bc32174da07545ff2c1a24f95af407a91caf5
jonatascp <jonatascp@gmail.com>
2017-06-17 10:05:55 -0300

    CRUD de contatos

--- routes/contato.js
+++ routes/contato.js
@@ -1,4 +1,9 @@
 module.exports = function(app) {
 	var contatos = app.controllers.contato;
 	app.get('/contatos', contatos.index);
+	app.get('/contato/:id', contatos.show);
+	app.post('/contato', contatos.create);
+	app.get('/contato/:id/editar', contatos.edit);
+	app.post('/contato/alterar/:id', contatos.update);
+	app.post('/contato/excluir/:id', contatos.destroy);
 };
